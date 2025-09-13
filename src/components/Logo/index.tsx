d35afb6a33ccb93c2aca509e7404010ce77f38fa
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2025-09-13 13:40:26 -0400

    Update index.tsx

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -15,7 +15,7 @@ export const Logo: FC = () => {
             {/* Logo */}
            <script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.7.1/dist/dotlottie-wc.js" type="module"></script>
 <dotlottie-wc src="https://lottie.host/8814880b-6dfe-461e-83f9-5531fecf248e/jRFeIx3iu0.lottie" speed="0.5" style="width: 60px; height: 60px" mode="forward" loop autoplay></dotlottie-wc>
-                <Image srcLocal="logo" alt="logo" />
+                <Image srcLocal="" alt="" />
             </a>
         </div>
     );

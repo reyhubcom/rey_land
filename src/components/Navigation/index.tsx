9c8997079547e7671e36590043ed0c0019655993
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2025-09-16 19:57:58 -0400

    Update index.tsx

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -13,7 +13,7 @@ export const Navigation: FC = () => {
              <script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.7.1/dist/dotlottie-wc.js" type="module"></script>
 <dotlottie-wc src="https://lottie.host/7602cf98-e8e9-4457-a1d8-f1d0887e3c94/WGNOBJacdh.lottie" speed="0.5" style="width: 50px; height: 50px" mode="forward" loop autoplay></dotlottie-wc>
                     
-                </Button>
+             
             </div>
         </nav>
     );

a5a42d8e17d2e77e1d896820cf20c97769546e11
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2025-09-16 20:41:49 -0400

    Update index.tsx

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -6,12 +6,13 @@ export const Navigation: FC = () => {
         <nav className="md:flex  md:grow">
             {/* Desktop sign in links */}
             <div className="flex items-center justify-end flex-wrap grow">
-               
+                <Button link="/market" variant="secondary">
+                    Market
+                </Button>
 
-             <script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.7.1/dist/dotlottie-wc.js" type="module"></script>
-<dotlottie-wc src="https://lottie.host/7602cf98-e8e9-4457-a1d8-f1d0887e3c94/WGNOBJacdh.lottie" speed="0.5" style="width: 50px; height: 50px" mode="forward" loop autoplay></dotlottie-wc>
-                    
-             
+                <Button link="/buy" variant="primary">
+                    Buy Crypto
+                </Button>
             </div>
         </nav>
     );
